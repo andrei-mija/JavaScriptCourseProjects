f161729208b9d11da241c3f220058c4b3bc13d5d
Andrei <A.Mija@student.tudelft.nl>
2020-07-21 01:13:41 +0300

    Added the 2 files in folders

---
+++ Timer/main.js
@@ -0,0 +1,27 @@
+class Timer{
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton =startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+    }
+
+    start() {
+        console.log("The timer has started");
+    }
+
+    pause() {
+        console.log("The timer has stopped");
+    }
+
+    tick() {
+        console.log("The timer has ticked");
+    }
+}
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);   
